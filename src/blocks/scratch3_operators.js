6f9889b84584a206af38879c7e55b21b4dd31999
Juan David Rodríguez García <juanda@juandarodriguez.es>
2019-04-03 07:49:57 +0200

    añado recibo de mensajes desde brain para actualizar el modelo de ML

--- src/blocks/scratch3_operators.js
+++ src/blocks/scratch3_operators.js
@@ -1,159 +1,173 @@
 const Cast = require('../util/cast.js');
 const MathUtil = require('../util/math-util.js');
 
+console.log('adding storage event');
+let brain = function(text){
+  return "no tengo modelo aún";
+};
+
+window.addEventListener("message", e => {
+  if (event.origin !== "http://localhost:8000")
+    return;
+  console.log(e);
+  code = e.data;
+  brain = new Function("rawInput", "maxPredictionLength", "isSampleI", "temperature", code);
+  console.log(brain);
+});
+
 class Scratch3OperatorsBlocks {
-    constructor (runtime) {
-        /**
-         * The runtime instantiating this block package.
-         * @type {Runtime}
-         */
-        this.runtime = runtime;
-    }
-
+  constructor(runtime) {
     /**
-     * Retrieve the block primitives implemented by this package.
-     * @return {object.<string, Function>} Mapping of opcode to Function.
+     * The runtime instantiating this block package.
+     * @type {Runtime}
      */
-    getPrimitives () {
-        return {
-            operator_recognize_term: this.recognizeTerm,
-            operator_add: this.add,
-            operator_subtract: this.subtract,
-            operator_multiply: this.multiply,
-            operator_divide: this.divide,
-            operator_lt: this.lt,
-            operator_equals: this.equals,
-            operator_gt: this.gt,
-            operator_and: this.and,
-            operator_or: this.or,
-            operator_not: this.not,
-            operator_random: this.random,
-            operator_join: this.join,
-            operator_letter_of: this.letterOf,
-            operator_length: this.length,
-            operator_contains: this.contains,
-            operator_mod: this.mod,
-            operator_round: this.round,
-            operator_mathop: this.mathop
-        };
-    }
+    this.runtime = runtime;
+  }
 
-    recognizeTerm (args) {
-        return args.STRING;
-    }
+  /**
+   * Retrieve the block primitives implemented by this package.
+   * @return {object.<string, Function>} Mapping of opcode to Function.
+   */
+  getPrimitives() {
+    return {
+      operator_recognize_term: this.recognizeTerm,
+      operator_add: this.add,
+      operator_subtract: this.subtract,
+      operator_multiply: this.multiply,
+      operator_divide: this.divide,
+      operator_lt: this.lt,
+      operator_equals: this.equals,
+      operator_gt: this.gt,
+      operator_and: this.and,
+      operator_or: this.or,
+      operator_not: this.not,
+      operator_random: this.random,
+      operator_join: this.join,
+      operator_letter_of: this.letterOf,
+      operator_length: this.length,
+      operator_contains: this.contains,
+      operator_mod: this.mod,
+      operator_round: this.round,
+      operator_mathop: this.mathop
+    };
+  }
 
-    add (args) {
-        return Cast.toNumber(args.NUM1) + Cast.toNumber(args.NUM2);
-    }
+  recognizeTerm(args) {
+    return brain(args.STRING);
+  }
 
-    subtract (args) {
-        return Cast.toNumber(args.NUM1) - Cast.toNumber(args.NUM2);
-    }
+  add(args) {
+    return Cast.toNumber(args.NUM1) + Cast.toNumber(args.NUM2);
+  }
 
-    multiply (args) {
-        return Cast.toNumber(args.NUM1) * Cast.toNumber(args.NUM2);
-    }
+  subtract(args) {
+    return Cast.toNumber(args.NUM1) - Cast.toNumber(args.NUM2);
+  }
 
-    divide (args) {
-        return Cast.toNumber(args.NUM1) / Cast.toNumber(args.NUM2);
-    }
+  multiply(args) {
+    return Cast.toNumber(args.NUM1) * Cast.toNumber(args.NUM2);
+  }
 
-    lt (args) {
-        return Cast.compare(args.OPERAND1, args.OPERAND2) < 0;
-    }
+  divide(args) {
+    return Cast.toNumber(args.NUM1) / Cast.toNumber(args.NUM2);
+  }
 
-    equals (args) {
-        return Cast.compare(args.OPERAND1, args.OPERAND2) === 0;
-    }
+  lt(args) {
+    return Cast.compare(args.OPERAND1, args.OPERAND2) < 0;
+  }
 
-    gt (args) {
-        return Cast.compare(args.OPERAND1, args.OPERAND2) > 0;
-    }
+  equals(args) {
+    return Cast.compare(args.OPERAND1, args.OPERAND2) === 0;
+  }
 
-    and (args) {
-        return Cast.toBoolean(args.OPERAND1) && Cast.toBoolean(args.OPERAND2);
-    }
+  gt(args) {
+    return Cast.compare(args.OPERAND1, args.OPERAND2) > 0;
+  }
 
-    or (args) {
-        return Cast.toBoolean(args.OPERAND1) || Cast.toBoolean(args.OPERAND2);
-    }
+  and(args) {
+    return Cast.toBoolean(args.OPERAND1) && Cast.toBoolean(args.OPERAND2);
+  }
 
-    not (args) {
-        return !Cast.toBoolean(args.OPERAND);
-    }
+  or(args) {
+    return Cast.toBoolean(args.OPERAND1) || Cast.toBoolean(args.OPERAND2);
+  }
 
-    random (args) {
-        const nFrom = Cast.toNumber(args.FROM);
-        const nTo = Cast.toNumber(args.TO);
-        const low = nFrom <= nTo ? nFrom : nTo;
-        const high = nFrom <= nTo ? nTo : nFrom;
-        if (low === high) return low;
-        // If both arguments are ints, truncate the result to an int.
-        if (Cast.isInt(args.FROM) && Cast.isInt(args.TO)) {
-            return low + Math.floor(Math.random() * ((high + 1) - low));
-        }
-        return (Math.random() * (high - low)) + low;
-    }
+  not(args) {
+    return !Cast.toBoolean(args.OPERAND);
+  }
 
-    join (args) {
-        return Cast.toString(args.STRING1) + Cast.toString(args.STRING2);
+  random(args) {
+    const nFrom = Cast.toNumber(args.FROM);
+    const nTo = Cast.toNumber(args.TO);
+    const low = nFrom <= nTo ? nFrom : nTo;
+    const high = nFrom <= nTo ? nTo : nFrom;
+    if (low === high) return low;
+    // If both arguments are ints, truncate the result to an int.
+    if (Cast.isInt(args.FROM) && Cast.isInt(args.TO)) {
+      return low + Math.floor(Math.random() * ((high + 1) - low));
     }
+    return (Math.random() * (high - low)) + low;
+  }
 
-    letterOf (args) {
-        const index = Cast.toNumber(args.LETTER) - 1;
-        const str = Cast.toString(args.STRING);
-        // Out of bounds?
-        if (index < 0 || index >= str.length) {
-            return '';
-        }
-        return str.charAt(index);
-    }
+  join(args) {
+    return Cast.toString(args.STRING1) + Cast.toString(args.STRING2);
+  }
 
-    length (args) {
-        return Cast.toString(args.STRING).length;
+  letterOf(args) {
+    const index = Cast.toNumber(args.LETTER) - 1;
+    const str = Cast.toString(args.STRING);
+    // Out of bounds?
+    if (index < 0 || index >= str.length) {
+      return '';
     }
+    return str.charAt(index);
+  }
 
-    contains (args) {
-        const format = function (string) {
-            return Cast.toString(string).toLowerCase();
-        };
-        return format(args.STRING1).includes(format(args.STRING2));
-    }
+  length(args) {
+    return Cast.toString(args.STRING).length;
+  }
 
-    mod (args) {
-        const n = Cast.toNumber(args.NUM1);
-        const modulus = Cast.toNumber(args.NUM2);
-        let result = n % modulus;
-        // Scratch mod is kept positive.
-        if (result / modulus < 0) result += modulus;
-        return result;
-    }
+  contains(args) {
+    const format = function (string) {
+      return Cast.toString(string).toLowerCase();
+    };
+    return format(args.STRING1).includes(format(args.STRING2));
+  }
 
-    round (args) {
-        return Math.round(Cast.toNumber(args.NUM));
-    }
+  mod(args) {
+    const n = Cast.toNumber(args.NUM1);
+    const modulus = Cast.toNumber(args.NUM2);
+    let result = n % modulus;
+    // Scratch mod is kept positive.
+    if (result / modulus < 0) result += modulus;
+    return result;
+  }
 
-    mathop (args) {
-        const operator = Cast.toString(args.OPERATOR).toLowerCase();
-        const n = Cast.toNumber(args.NUM);
-        switch (operator) {
-        case 'abs': return Math.abs(n);
-        case 'floor': return Math.floor(n);
-        case 'ceiling': return Math.ceil(n);
-        case 'sqrt': return Math.sqrt(n);
-        case 'sin': return parseFloat(Math.sin((Math.PI * n) / 180).toFixed(10));
-        case 'cos': return parseFloat(Math.cos((Math.PI * n) / 180).toFixed(10));
-        case 'tan': return MathUtil.tan(n);
-        case 'asin': return (Math.asin(n) * 180) / Math.PI;
-        case 'acos': return (Math.acos(n) * 180) / Math.PI;
-        case 'atan': return (Math.atan(n) * 180) / Math.PI;
-        case 'ln': return Math.log(n);
-        case 'log': return Math.log(n) / Math.LN10;
-        case 'e ^': return Math.exp(n);
-        case '10 ^': return Math.pow(10, n);
-        }
-        return 0;
+  round(args) {
+    return Math.round(Cast.toNumber(args.NUM));
+  }
+
+  mathop(args) {
+    const operator = Cast.toString(args.OPERATOR).toLowerCase();
+    const n = Cast.toNumber(args.NUM);
+    switch (operator) {
+      case 'abs': return Math.abs(n);
+      case 'floor': return Math.floor(n);
+      case 'ceiling': return Math.ceil(n);
+      case 'sqrt': return Math.sqrt(n);
+      case 'sin': return parseFloat(Math.sin((Math.PI * n) / 180).toFixed(10));
+      case 'cos': return parseFloat(Math.cos((Math.PI * n) / 180).toFixed(10));
+      case 'tan': return MathUtil.tan(n);
+      case 'asin': return (Math.asin(n) * 180) / Math.PI;
+      case 'acos': return (Math.acos(n) * 180) / Math.PI;
+      case 'atan': return (Math.atan(n) * 180) / Math.PI;
+      case 'ln': return Math.log(n);
+      case 'log': return Math.log(n) / Math.LN10;
+      case 'e ^': return Math.exp(n);
+      case '10 ^': return Math.pow(10, n);
     }
+    return 0;
+  }
 }
 
 module.exports = Scratch3OperatorsBlocks;
