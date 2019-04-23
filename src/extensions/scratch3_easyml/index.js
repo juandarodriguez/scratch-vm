c87ecc91d40a04319ae0d2321c704a22bcb77e4a
Juan David Rodríguez García <juanda@MacBook-Pro-de-Juan.local>
2019-04-23 21:09:02 +0200

    cambiando el mecanismo para obtener el modelo de easyML, ahora es la extensión de scratch ML la que pide el modelo a easyML

--- src/extensions/scratch3_easyml/index.js
+++ src/extensions/scratch3_easyml/index.js
@@ -3,46 +3,28 @@ const BlockType = require('../../extension-support/block-type');
 const Cast = require('../../util/cast');
 const log = require('../../util/log');
 const brain = require('brain.js');
-const { BagOfWords } = require('./bag-of-words')
-
-let allowedDomains = [
-    "http://localhost:4200",
-    "http://juandarodriguez.es"
-]
-
-let run = function (entry) {
-    return {
-        label: 'NO LABEL: you have to train your model',
-        confidence: 'NO CONFIDENCE: you have to train your model'
-    }
-};
-
+const { BagOfWords } = require('./bag-of-words');
+const createGuest = require('cross-domain-storage/guest');
 
 
 class Scratch3Easyml {
+
     constructor(runtime) {
-        this.loadedFromDisk = false;
+        this.modelFunction = null;
+        this.easymodelStorage = createGuest("http://localhost:4200");
         this.runtime = runtime;
-        window.addEventListener("message", () => {
-            if (allowedDomains.indexOf(event.origin) != -1) {
-                console.log(event);
-                this.runtime.easyml_model = event.data;
-                this.buildModel(event.data);
-            }
-        }, false);
     }
 
-    buildModel(data) {
-        let dataObj = JSON.parse(data);
-        let modelJSON = dataObj.modelJSON;
-        let classes = dataObj.classes;
-        let dict = dataObj.dict;
+    buildModel(modelObj) {
+        let modelJSON = modelObj.modelJSON;
+        let classes = modelObj.classes;
+        let dict = modelObj.dict;
 
         let net = new brain.NeuralNetwork();
         net.fromJSON(modelJSON);
         let bow = new BagOfWords();
 
-        run = function (entry) {
+        this.modelFunction = function (entry) {
             let term = bow.bow(entry, dict);
             let predict = net.run(term);
             let i = bow.maxarg(predict);
@@ -74,9 +56,9 @@ class Scratch3Easyml {
             name: 'EasyML',
             blocks: [
                 {
-                    opcode: 'clasify',
+                    opcode: 'classify',
                     blockType: BlockType.REPORTER,
-                    text: 'Clasify [TEXT]',
+                    text: 'Classify [TEXT]',
                     arguments: {
                         TEXT: {
                             type: ArgumentType.STRING,
@@ -102,18 +84,44 @@ class Scratch3Easyml {
         };
     }
 
-    clasify(args) {
-        if (this.runtime.easyml_model && !this.loadedFromDisk) {
-            this.buildModel(this.runtime.easyml_model);
-            this.loadedFromDisk = true;
+    sincroLocalModel(model){
+        if(!this.runtime.easyml_model || model.id != this.runtime.easyml_model.id){
+            this.runtime.easyml_model = model;
+            this.modelFunction = null; // force to update the modelFunction
         }
-        const text = Cast.toString(args.TEXT);
-        return run(text)['label']
+    }
+
+    sincroModelFunction(){
+        if(this.modelFunction == null){
+            this.buildModel(this.runtime.easyml_model);
+        }
+    }
+
+    classify(args) {
+        
+        return new Promise((resolve, reject) => {
+            this.easymodelStorage.get("easyml_model",  (error, model) => {
+                if (error) {
+                    reject(error);
+                }
+                let modelObj = JSON.parse(model);
+                console.log(modelObj);
+                this.sincroLocalModel(modelObj);
+                this.sincroModelFunction();
+
+                const text = Cast.toString(args.TEXT);
+
+                result = this.modelFunction(text)['label']
+
+                resolve(result);
+            })
+        });
+
     }
 
     confidence(args) {
         const text = Cast.toString(args.TEXT);
-        return run(text)['confidence']
+        return this.modelFunction(text)['confidence']
     }
 
 }
