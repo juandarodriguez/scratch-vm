5479b5b13c92cf6ad690af9002a0425ae6df7857
Juan David Rodríguez García <juanda@MacBook-Pro-de-Juan.local>
2019-04-24 07:34:49 +0200

    cambiado el mecanismo para la obtención del modelo basado en acceder al localstorage de easyML desde otro dominio (cross domain localstorage)

--- src/extensions/scratch3_easyml/index.js
+++ src/extensions/scratch3_easyml/index.js
@@ -16,11 +16,21 @@ class Scratch3Easyml {
     }
 
     buildModel(modelObj) {
+        console.log("entro en buildModel");
         let modelJSON = modelObj.modelJSON;
         let classes = modelObj.classes;
         let dict = modelObj.dict;
 
+        // Atention TRICK: When serialized, if timeout=Infinity, as JSON don't 
+        // understand Infinity value is saved as 0 which causes an error when 
+        // building net fromJSON. So, this is fixed here (I don't like this solution)
+        if (modelJSON.trainOpts.timeout != undefined) {
+            modelJSON.trainOpts.timeout =
+                (modelJSON.trainOpts.timeout == 0) ? Infinity : modelJSON.trainOpts.timeout;
+        }
+
         let net = new brain.NeuralNetwork();
+
         net.fromJSON(modelJSON);
         let bow = new BagOfWords();
 
@@ -84,44 +94,88 @@ class Scratch3Easyml {
         };
     }
 
-    sincroLocalModel(model){
-        if(!this.runtime.easyml_model || model.id != this.runtime.easyml_model.id){
+    sincroLocalModel(model) {
+        console.log(this.runtime.easyml_model);
+        console.log(model)
+
+        // these conditional chain can be simplified. However I prefer keep it 
+        // like this because is easier to be interpreted
+        if (model && this.runtime.easyml_model) {
+            // this.runtime.easyml_model has to be updated with model
+            // if they are diferent
+            if (model.id != this.runtime.easyml_model.id) {
+                this.runtime.easyml_model = model;
+                this.modelFunction = null; // force to update the modelFunction
+            }
+        } else if (model && !this.runtime.easyml_model) {
+            // this.runtime.easyml_model has to be updated
             this.runtime.easyml_model = model;
             this.modelFunction = null; // force to update the modelFunction
+        } else if (!model && this.runtime.easyml_model) {
+            // nothing to do
+            console.log("nothing to do");
+
+        } else { // !model && !this.runtime.easyml_model
+            // nothing to do
+            console.log("nothing to do");
         }
     }
 
-    sincroModelFunction(){
-        if(this.modelFunction == null){
+    sincroModelFunction() {
+        if (this.modelFunction == null) {
+            console.log("building modelFunction with this model");
+            console.log(this.runtime.easyml_model);
             this.buildModel(this.runtime.easyml_model);
         }
     }
 
-    classify(args) {
-        
+    getModelFromEasyML() {
         return new Promise((resolve, reject) => {
-            this.easymodelStorage.get("easyml_model",  (error, model) => {
+            this.easymodelStorage.get("easyml_model", (error, model) => {
                 if (error) {
                     reject(error);
+                    return;
                 }
                 let modelObj = JSON.parse(model);
                 console.log(modelObj);
                 this.sincroLocalModel(modelObj);
-                this.sincroModelFunction();
-
-                const text = Cast.toString(args.TEXT);
-
-                result = this.modelFunction(text)['label']
-
-                resolve(result);
+                if (!this.runtime.easyml_model) {
+                    reject("You must train a model before using me");
+                } else {
+                    this.sincroModelFunction();
+                    resolve(true);
+                }
             })
         });
+    }
 
+    classify(args) {
+
+        return this.getModelFromEasyML().then(
+            r => {
+                const text = Cast.toString(args.TEXT);
+                let result = this.modelFunction(text)['label']
+                return result;
+            },
+            e => {
+                return e;
+            }
+        );
     }
 
     confidence(args) {
-        const text = Cast.toString(args.TEXT);
-        return this.modelFunction(text)['confidence']
+
+        return this.getModelFromEasyML().then(
+            r => {
+                const text = Cast.toString(args.TEXT);
+                let result = this.modelFunction(text)['confidence']
+                return result;
+            },
+            e => {
+                return e;
+            }
+        );
+
     }
 
 }
